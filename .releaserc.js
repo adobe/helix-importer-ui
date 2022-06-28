e1869962e89289cad99744fac3fe8fed834b795d
Alexandre Capt <acapt@adobe.com>
2022-06-28 17:26:19 +0200

    feat: add semantic release

---
+++ .releaserc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/changelog', {
+      'changelogFile': 'CHANGELOG.md',
+    }],
+    ["@semantic-release/npm", {
+      npmPublish: false,
+    }],
+    ['@semantic-release/git', {
+      'assets': ['package.json', 'package-lock.json', 'CHANGELOG.md', 'js/dist/spectrum-web-components.js', 'js/dist/helix-importer.js'],
+      'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+    }],
+    ['@semantic-release/github', {}],
+  ],
+  branches: ['main'],
+};
