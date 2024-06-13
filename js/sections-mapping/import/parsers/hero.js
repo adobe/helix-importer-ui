fa40a82262a7d483589139a7a8b77e252c4ad192
david catalan <catalan@adobe.com>
2024-06-13 11:52:49 +0200

    fix: parse background image for hero block

--- js/sections-mapping/import/parsers/hero.js
+++ js/sections-mapping/import/parsers/hero.js
@@ -1,5 +1,13 @@
+import { extractBackground } from '../import.utils.js';
+
 /* globals WebImporter */
 export default function heroParser(el, { mapping, document }) {
+  const imgEl = extractBackground(el, document, { strategy: 'image' }) || null;
+  console.log('imgEl', imgEl);
+
+  if (imgEl) {
+    el.prepend(imgEl);
+  }
   return WebImporter.DOMUtils.createTable([
     ['hero'],
     [el],
