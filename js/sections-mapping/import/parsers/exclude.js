5848b83056f8a6577d9a24cd7fcd92b683337052
david catalan <catalan@adobe.com>
2024-06-13 09:24:31 +0200

    chore: remove obsolete parser to exclude content

--- js/sections-mapping/import/parsers/exclude.js
+++
@@ -1,3 +0,0 @@
-export default function excludeParser(el/* , window */) {
-  el.remove();
-}
--- js/sections-mapping/import/parsers/parsers.js
+++ js/sections-mapping/import/parsers/parsers.js
@@ -2,14 +2,12 @@
  * exports parsers
  */
 
-import exclude from './exclude.js';
 import columns from './columns.js';
 import defaultContent from './default-content.js';
 import header from './header.js';
 import footer from './footer.js';
 
 export {
-  exclude,
   columns,
   defaultContent,
   header,
