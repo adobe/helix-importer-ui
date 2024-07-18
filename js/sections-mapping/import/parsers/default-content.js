95f95390f9421751d2c030f622151eca97ce83f0
David Catalan <40354404+catalan-adobe@users.noreply.github.com>
2024-07-18 16:41:17 +0200

    Feat(sm-import): support custom block name in default content parser (#402)
    * style(sm-import): minor improvement
    
    * feat(sm-import): support custom block name in default content parser
    
    * chore(sm-import): linting

--- js/sections-mapping/import/parsers/default-content.js
+++ js/sections-mapping/import/parsers/default-content.js
@@ -1,3 +1,12 @@
+/* globals WebImporter */
+
 export default function defaultContentParser(el, { mapping, document }) {
+  if (mapping.customBlockName) {
+    return WebImporter.DOMUtils.createTable([
+      [mapping.customBlockName],
+      [el],
+    ], document);
+  }
+
   return el;
 }
