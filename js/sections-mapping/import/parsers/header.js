ea47c188b92dcb657e1747d3819f88d8ec3c0fa3
david catalan <catalan@adobe.com>
2024-08-19 09:02:53 +0200

    refactor(sm-import): extract brand logo extraction outside of main parser function

--- js/sections-mapping/import/parsers/header.js
+++ js/sections-mapping/import/parsers/header.js
@@ -1,75 +1,69 @@
-export default function headerParser(el, { document, params, allMappings }) {
-  const containerEl = document.createElement('div');
-
-  const brandEl = document.createElement('div');
-  const bodyWidth = allMappings.sections[0]?.blocks[0]?.width;
-  const originURL = new URL(params.originalURL).origin;
-
-  const brandLogoMapping = [
-    {
-      checkFn: (e) => e.querySelector('a > picture, a > img'),
-      parseFn: (e, targetEl, x) => {
-        if (bodyWidth && x < bodyWidth / 2) {
-          targetEl.append(e);
-          return true;
-        }
-        return false;
-      },
-    },
-    {
-      checkFn: (e) => e.querySelector('picture + br + a, img + br + a'),
-      parseFn: (e, targetEl, x) => {
-        if (bodyWidth && x < bodyWidth / 2) {
-          const imgEl = e.closest('picture, img');
-          if (imgEl) {
-            targetEl.append(imgEl);
-          }
-          return true;
-        }
-        return false;
-      },
-    },
-    {
-      checkFn: (e) => e.querySelector('img'),
-      parseFn: (e, targetEl, x) => {
-        if (bodyWidth && x < bodyWidth / 2) {
-          targetEl.append(e);
-          return true;
-        }
-        return false;
-      },
-    },
-    {
-      checkFn: (e) => e.querySelector(`a[href="/"], a[href="${originURL}"], a[href="${originURL}/"]`),
-      parseFn: (e, targetEl, x) => {
-        if (bodyWidth && x < bodyWidth / 2) {
-          targetEl.append(e);
-          return true;
-        }
-        return false;
-      },
-    },
-    {
-      checkFn: (e) => {
-        // fetch favicon
-        const resp = fetch('/favicon.ico');
-        if (resp && resp.status === 200) {
-          const logoEl = document.createElement('img');
-          logoEl.src = '/favicon.ico';
-          return logoEl;
-        }
-        return null;
-      },
-      parseFn: (e, targetEl) => {
+const brandLogoMapping = [
+  {
+    checkFn: (e) => e.querySelector('a > picture, a > img'),
+    parseFn: (e, targetEl, bodyWidth, x) => {
+      if (bodyWidth && x < bodyWidth / 2) {
         targetEl.append(e);
         return true;
-      },
+      }
+      return false;
     },
-  ];
+  },
+  {
+    checkFn: (e) => e.querySelector('picture + br + a, img + br + a'),
+    parseFn: (e, targetEl, bodyWidth, x) => {
+      if (bodyWidth && x < bodyWidth / 2) {
+        const imgEl = e.closest('picture, img');
+        if (imgEl) {
+          targetEl.append(imgEl);
+        }
+        return true;
+      }
+      return false;
+    },
+  },
+  {
+    checkFn: (e) => e.querySelector('img'),
+    parseFn: (e, targetEl, bodyWidth, x) => {
+      if (bodyWidth && x < bodyWidth / 2) {
+        targetEl.append(e);
+        return true;
+      }
+      return false;
+    },
+  },
+  {
+    checkFn: (e, { originURL }) => e.querySelector(`a[href="/"], a[href="${originURL}"], a[href="${originURL}/"]`),
+    parseFn: (e, targetEl, bodyWidth, x) => {
+      if (bodyWidth && x < bodyWidth / 2) {
+        targetEl.append(e);
+        return true;
+      }
+      return false;
+    },
+  },
+  {
+    checkFn: () => {
+      // fetch favicon
+      const resp = fetch('/favicon.ico');
+      if (resp && resp.status === 200) {
+        const logoEl = document.createElement('img');
+        logoEl.src = '/favicon.ico';
+        return logoEl;
+      }
+      return null;
+    },
+    parseFn: (e, targetEl) => {
+      targetEl.append(e);
+      return true;
+    },
+  },
+];
 
-  // get brand logo
+function getBrandLogo(rootEl, document, { bodyWidth, originURL }) {
+  const brandEl = document.createElement('div');
   brandLogoMapping.some((m) => {
-    const logoEl = m.checkFn(el);
+    const logoEl = m.checkFn(rootEl, { originURL });
     if (logoEl) {
       let x = 0;
       try {
@@ -78,10 +72,22 @@ export default function headerParser(el, { document, params, allMappings }) {
         console.error('error', e);
       }
 
-      return m.parseFn(logoEl, brandEl, x);
+      return m.parseFn(logoEl, brandEl, bodyWidth, x);
     }
     return false;
   });
+
+  return brandEl;
+}
+
+export default function headerParser(el, { document, params, allMappings }) {
+  const containerEl = document.createElement('div');
+
+  const bodyWidth = allMappings.sections[0]?.blocks[0]?.width;
+  const originURL = new URL(params.originalURL).origin;
+
+  // get brand logo
+  const brandEl = getBrandLogo(el, document, { bodyWidth, originURL });
 
   // get navigation content
   const navEl = document.createElement('div');
