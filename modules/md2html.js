2fdf960e0e3829463d07fe89de246697d1432020
kptdobe <acapt@adobe.com>
2025-06-24 14:59:40 +0200

    chore: no code copy

--- modules/md2html.js
+++ modules/md2html.js
@@ -14,134 +14,13 @@ import { toHast as mdast2hast, defaultHandlers } from 'mdast-util-to-hast';
 import { raw } from 'hast-util-raw';
 import remarkGridTable from '@adobe/remark-gridtables';
 import { mdast2hastGridTablesHandler, TYPE_TABLE } from '@adobe/mdast-util-gridtables';
-import { h } from 'hastscript';
 import { toHtml } from 'hast-util-to-html';
-import { select } from 'hast-util-select';
-import { toString } from 'hast-util-to-string';
-import { CONTINUE, SKIP, visit } from 'unist-util-visit';
 import rehypeFormat from 'rehype-format';
 import { unified } from 'unified';
 import remarkParse from 'remark-parse';
 import remarkGfm from 'remark-gfm';
-
-/* start: copied from helix-html-pipeline */
-function childNodes(node) {
-  return node.children.filter((n) => n.type === 'element');
-}
-
-function toBlockCSSClassNames(text) {
-  if (!text) {
-    return [];
-  }
-  const names = [];
-  const idx = text.lastIndexOf('(');
-  if (idx >= 0) {
-    names.push(text.substring(0, idx));
-    names.push(...text.substring(idx + 1).split(','));
-  } else {
-    names.push(text);
-  }
-
-  return names.map((name) => name
-    .toLowerCase()
-    .replace(/[^0-9a-z]+/g, '-')
-    .replace(/^-+/, '')
-    .replace(/-+$/, ''))
-    .filter((name) => !!name);
-}
-
-function tableToDivs($table, wrap = false) {
-  const $cards = h('div');
-  const $rows = [];
-  // eslint-disable-next-line no-restricted-syntax
-  for (const child of $table.children) {
-    if (child.tagName === 'thead' || child.tagName === 'tbody') {
-      $rows.push(...childNodes(child));
-    }
-  }
-
-  if ($rows.length === 0) {
-    return $cards;
-  }
-  const $headerCols = childNodes($rows.shift());
-  if ($headerCols.length === 0) {
-    return $cards;
-  }
-
-  // special case, only 1 row and 1 column with a nested table
-  if ($rows.length === 0 && $headerCols.length === 1) {
-    const $nestedTable = select(':scope table', $headerCols[0]);
-    if ($nestedTable) {
-      return $nestedTable;
-    }
-  }
-
-  // get columns names
-  $cards.properties.className = toBlockCSSClassNames(toString($headerCols[0]));
-  if ($cards.properties.className.length === 0) {
-    delete $cards.properties.className;
-  }
-
-  // construct page block
-  // eslint-disable-next-line no-restricted-syntax
-  for (const $row of $rows) {
-    const $card = h('div');
-    // eslint-disable-next-line no-restricted-syntax
-    for (const $cell of childNodes($row)) {
-      // convert to div
-      $card.children.push(h('div', {
-        'data-align': $cell.properties.align,
-        'data-valign': $cell.properties.vAlign,
-      }, $cell.children));
-    }
-    $cards.children.push($card);
-  }
-
-  if (wrap) {
-    return h('div', {}, $cards);
-  }
-  return $cards;
-}
-
-function createPageBlocks({ content }) {
-  const { hast } = content;
-  visit(hast, (node, idx, parent) => {
-    if (node.tagName === 'table') {
-      parent.children[idx] = tableToDivs(node, parent.tagName !== 'div');
-      return SKIP;
-    }
-    return CONTINUE;
-  });
-}
-
-function split(state) {
-  const { content: { mdast } } = state;
-
-  // filter all children that are break blocks
-  const dividers = mdast.children.filter((node) => node.type === 'thematicBreak')
-    // then get their index in the list of children
-    .map((node) => mdast.children.indexOf(node));
-
-  // find pairwise permutations of spaces between blocks
-  // include the very start and end of the document
-  const starts = [0, ...dividers];
-  const ends = [...dividers, mdast.children.length];
-
-  // content.mdast.children = _.zip(starts, ends)
-  mdast.children = starts.map((k, i) => [k, ends[i]])
-    // but filter out empty section
-    .filter(([start, end]) => start !== end)
-    // then return all nodes that are in between
-    .map(([start, end]) => {
-      // skip 'thematicBreak' nodes
-      const index = mdast.children[start].type === 'thematicBreak' ? start + 1 : start;
-      return {
-        type: 'section',
-        children: mdast.children.slice(index, end),
-      };
-    });
-}
-/* end: copied from helix-html-pipeline */
+import split from '@adobe/helix-html-pipeline/src/steps/split-sections.js';
+import createPageBlocks from '@adobe/helix-html-pipeline/src/steps/create-page-blocks.js';
 
 export default function md2html(md, da = false) {
   // note: we could use the entire unified chain, but it would need to be async -
